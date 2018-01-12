6d938ae61b9926ce94c5a2bd72bcac2521dc0b21
Huang <huang.z@watstock.com>
2018-01-12 13:55:01 +0800

    update reading and testing

--- fast_read_data.py
+++ fast_read_data.py
@@ -5,146 +5,120 @@ from sklearn.utils import shuffle
 import numpy as np
 import struct
 from PIL import Image
-
-from sklearn.preprocessing import LabelBinarizer
-lb = LabelBinarizer()
-
-
-ONLY_USE_100_CHARS = True
-CHARS_100_LIST = None
-CHARS_100_LIST_BINARIZED = None
-if CHARS_100_LIST is None:
-    CHARS_100_LIST = random.sample(range(0,3755),100)
-    print 'selected chars are:',CHARS_100_LIST
-    lb.fit(CHARS_100_LIST)
-    CHARS_100_LIST_BINARIZED = lb.transform(CHARS_100_LIST)
+import pickle
+import time
 
 
 class DataSet:
-    global ONLY_USE_100_CHARS
-    global CHARS_100_LIST
-    global lb
 
-    def __init__(self, path):
+    def __init__(self, path, is_train=True, char_dict=None):
+        self.file_counter = 0
+        self.is_train = is_train
         self.iter_index = 0
-        if ONLY_USE_100_CHARS:
-            self.files, self.labels = self.get_100_input_lists(path)
-        else:
-            self.files, self.labels = self.get_all_input_lists(path)
-        self.files, self.labels = shuffle(self.files, self.labels)
+        self.path = path
+        self.char_dict = char_dict
 
-    def get_100_input_lists(self, data_dir):
-        image_names = []
-        label_list = []
-        for root, sub_folder, file_list in os.walk(data_dir):
-            # print root, sub_folder, file_list
+    def one_file(self, f):
+        header_size = 10
+        while True:
+            header = np.fromfile(f, dtype='uint8', count=header_size)
+            if not header.size: break
+            sample_size = header[0] + (header[1] << 8) + (header[2] << 16) + (header[3] << 24)
+            tagcode = header[5] + (header[4] << 8)
+            width = header[6] + (header[7] << 8)
+            height = header[8] + (header[9] << 8)
+            if header_size + width * height != sample_size:
+                break
             try:
-                test_num = int(root[-5:])
+                image = np.fromfile(f, dtype='uint8', count=width * height).reshape((height, width))
+                # image = Image.fromarray(image)
             except:
-                print 'this is not a data folder, skipped'
-                continue
-            if test_num in CHARS_100_LIST:
-                image_names += [os.path.join(root, file_path) for file_path in file_list]
-                for i in range(len(file_list)):
-                    label_list.append(test_num)
-        return image_names, label_list
+                print struct.pack('>H', tagcode).decode('gb2312')
+            yield image, tagcode
 
-    @staticmethod
-    def read_from_gnt_dir(gnt_dir):
-        def one_file(f):
-            header_size = 10
-            while True:
-                header = np.fromfile(f, dtype='uint8', count=header_size)
-                if not header.size: break
-                sample_size = header[0] + (header[1] << 8) + (header[2] << 16) + (header[3] << 24)
-                tagcode = header[5] + (header[4] << 8)
-                width = header[6] + (header[7] << 8)
-                height = header[8] + (header[9] << 8)
-                if header_size + width * height != sample_size:
-                    break
-                try:
-                    image = np.fromfile(f, dtype='uint8', count=width * height).reshape((height, width))
-                except:
-                    print struct.pack('>H', tagcode).decode('gb2312')
-                yield image, tagcode
-
+    def read_from_gnt_dir(self, gnt_dir):
         for file_name in os.listdir(gnt_dir):
             if file_name.endswith('.gnt'):
                 file_path = os.path.join(gnt_dir, file_name)
                 with open(file_path, 'rb') as f:
-                    for image, tagcode in one_file(f):
+                    for image, tagcode in self.one_file(f):
                         yield image,tagcode
 
+    def read_one_gnt_file(self):
+        for file_name in os.listdir(self.path):
+            if file_name.endswith('.gnt'):
+                file_path = os.path.join(self.path, file_name)
+                with open(file_path, 'rb') as f:
+                    x = []
+                    y = []
+                    for image, tagcode in self.one_file(f):
+                        x.append(image)
+                        y.append(tagcode)
+                yield x, y
 
-    def get_all_input_lists(self, data_dir):
-        image_names = []
-        label_list = []
-        label_len = []
-        for root, sub_folder, file_list in os.walk(data_dir):
-            image_names += [os.path.join(root, file_path) for file_path in file_list]
-            for name in sub_folder:
-                current_dir = os.path.join(root, name)
-                set_images = os.listdir(current_dir)
-                label_len.append(len(set_images))
-        index = 0
-        while index < (len(label_len)):
-            i = 0
-            while i in range(label_len[index]):
-                label_list.append(index)
-                i += 1
-            index += 1
-        return image_names, label_list
+    #todo: to unittest
+    def load_next_file(self):
+        for x_s,y_s in self.read_one_gnt_file():
+            with tf.Session() as sess:
+                result_x = []
+                result_y = []
+                for i in range(len(x_s)):
+                    result = self.read_convert_image(x_s[i])
+                    result_x.append(result)
+                    result_y.append(y_s[i])
+            x = np.array(result_x)
+            y = np.array(result_y)
+            self.file_counter += 1
+            print ('loaded files count',self.file_counter)
+            yield x, y
 
-    def load_next_batch(self, batch_size=200,flatten=False):
-        for image, tagcode in self.read_from_gnt_dir(gnt_dir=train_data_dir):
-            tagcode_unicode = struct.pack('>H', tagcode).decode('gb2312')
-            im = Image.fromarray(image)
-            dir_name = './data/train/' + '%0.5d' % char_dict[tagcode_unicode]
-            if not os.path.exists(dir_name):
-                os.makedirs(dir_name)
-            im.convert('RGB').save(dir_name + '/' + str(train_counter) + '.png')
-            train_counter += 1
-            print '%d/%d' % (train_counter, file_count_train * 3755)
+    def load_all(self):
+        all_size = len(os.listdir(self.path))
         x = []
         y = []
-        with tf.Session() as sess:
-            for i in range(batch_size):
-                index = self.iter_index + i
-                oper = self.read_convert_image(self.files[index])
-                # result = sess.run(oper)
-                # if flatten:
-                #     result = result.flatten
-                x.append(oper)
-                y.append(self.labels[index])
-            x = sess.run(x)
-            x = np.array(x)
-            if flatten:
-                x = x.flatten().reshape(batch_size,-1)
-        self.iter_index += batch_size
-        x = np.array(x)
-        y = np.array(y)
-        y = lb.transform(y)
-        return x, y
+        for temp_x,temp_y in self.load_next_file():
+            x.extend(temp_x)
+            y.extend(temp_y)
+        return np.array(x), np.array(y)
 
-    def load_all(self, flatten=False):
-        all_size = len(self.labels)
-        x,y = self.load_next_batch(all_size,flatten)
-        self.iter_index = 0
-        return x,y
 
-    def read_convert_image(self, image_name):
-        images_content = tf.read_file(image_name)
-        image = tf.image.convert_image_dtype(tf.image.decode_png(images_content, channels=1), tf.float32)
-        new_size = tf.constant([64, 64], dtype=tf.int32)
-        new_image = tf.image.resize_images(image, new_size)
+    def read_convert_image(self, image):
+        im = Image.fromarray(image)
+        im = im.resize([64,64])
+        new_image = np.asarray(im)
+        new_image = new_image.reshape(new_image.shape[0], new_image.shape[1], 1)
+        # print type(new_image)
+        # im.show()
+        # image_tensor = tf.image.convert_image_dtype(image, tf.float32)
+        # new_size = tf.constant([64, 64], dtype=tf.int32)
+        # new_image = tf.image.resize_images(image_tensor, new_size)
         return new_image
 
 
 class ChineseWrittenChars:
     def __init__(self):
-        self.train = DataSet('HWDB1.1trn_gnt')
-        self.test = DataSet('HWDB1.1tst_gnt')
+        self.train = DataSet('HWDB1.1trn_gnt',is_train=True)
+
+        # self.char_dict = self.generate_char_dict()
+        print('--------create char_dict successfully------------')
+
+        self.test = DataSet('HWDB1.1tst_gnt',is_train=False)
         print 'inited datasets successfully'
+
+    def generate_char_list(self):
+        if os.path.isfile('char_list'):
+            with open('char_list', 'rb') as f:
+                print 'char dict had been generated, just load'
+                char_list = pickle.load(f)
+                return char_list
+        else:
+            char_list = []
+            for _, tagcode in self.train.read_from_gnt_dir(gnt_dir='HWDB1.1trn_gnt'):
+                char_list.append(tagcode)
+            with open('char_list', 'wb') as f:
+                pickle.dump(char_list,f)
+            return char_list
+
 #
 # loader = ChineseWrittenChars()
 # import time
---
+++ trainin_with_Chinese.py
@@ -0,0 +1,99 @@
+# from read_data import *
+import tensorflow as tf
+import time
+# from keras.datasets import mnist
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelBinarizer
+
+
+# from kerlayers import Conv2D, MaxPooling2D, ZeroPadding2D, GlobalAveragePooling2D
+if tf.__version__ == '1.2.0':
+    from tensorflow.contrib.keras.python.keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, GlobalAveragePooling2D, Dropout, Activation, Flatten, Dense
+    from tensorflow.contrib.keras.python.keras.layers.normalization import BatchNormalization
+    # from keras.models import Sequential
+    from tensorflow.contrib.keras.api.keras.models import Sequential
+    from tensorflow.contrib.keras.python.keras.utils import np_utils
+else:
+    from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, \
+        GlobalAveragePooling2D, Dropout, Activation, Flatten, Dense
+    from keras.layers.normalization import BatchNormalization
+    from keras.models import Sequential
+    from keras.utils import np_utils
+
+
+from fast_read_data import ChineseWrittenChars
+
+chars = ChineseWrittenChars()
+lb = LabelBinarizer()
+lb.fit(chars.generate_char_list())
+number_of_classes = 3755
+
+def build_model():
+    model = Sequential()
+
+    model.add(Conv2D(64, (3, 3), input_shape=(64, 64, 1)))
+    model.add(BatchNormalization(axis=-1))
+    model.add(Activation('relu'))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(BatchNormalization(axis=-1))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(BatchNormalization(axis=-1))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(BatchNormalization(axis=-1))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+
+    # Fully connected layer
+    model.add(Dense(256))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(3755))
+
+    model.add(Activation('softmax'))
+    return model
+
+
+def training(X_train,y_train):
+    model = build_model()
+    model.compile(optimizer='adam', loss='categorical_crossentropy')
+    model.summary()
+    model.fit(X_train, y_train, epochs=10)
+    model.save('model.h5')
+
+
+def inference(X_test, y_test):
+    # load model
+    from keras.models import load_model
+    model = load_model('model.h5')
+    model.compile(optimizer='adam',loss='categorical_crossentropy')
+    loaded_model_score = model.evaluate(X_test, y_test)
+    print('Test accuracy: ', loaded_model_score)
+
+
+def app(train_or_test):
+    if train_or_test == 'train':
+        start_time = time.time()
+        X_train, y_train = chars.train.load_all()
+        X_train /= 255
+        y_train = lb.transform(y_train)
+        print 'load training used time:', time.time() - start_time
+        print X_train.shape
+        print y_train.shape
+        training(X_train, y_train)
+
+    if train_or_test == 'test':
+        X_test, y_test = chars.test.load_all()
+        y_test = lb.transform(y_test)
+        X_test /= 255
+        inference(X_test, y_test)
+
+app('train')
+# app('test')
+
